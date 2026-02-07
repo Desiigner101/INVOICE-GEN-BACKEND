eb6902ba0725b5a532ffc30dd830e5bf46a97ff3
Kervin Gino Sarsonas <Desiigner4074@gmail.com>
2026-02-07 10:35:15 +0800

    fix: added explicit instructions to mongo config, to fix the "test" folder issue

---
+++ invoice-backend/src/main/java/com/invoicegen/invoice_backend/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package com.invoicegen.invoice_backend.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "invoicedb";
+    }
+
+    @Override
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create("mongodb://localhost:27017");
+    }
+}
